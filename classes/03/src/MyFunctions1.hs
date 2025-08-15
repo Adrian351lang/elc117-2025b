53f17547dda65739e02ae28d3b21fa46433bb197
Adrian351lang <177791808+Adrian351lang@users.noreply.github.com>
2025-08-15 23:56:41 +0300

    Update MyFunctions1.hs

--- classes/03/src/MyFunctions1.hs
+++ classes/03/src/MyFunctions1.hs
@@ -1,32 +1,37 @@
 module MyFunctions1 where
 
-
--- 1. Crie uma função `sumSquares :: Int -> Int -> Int` que receba dois números x e y e calcule a soma dos seus quadrados.
 sumSquares :: Int -> Int -> Int
-sumSquares x y = -- complete aqui
+sumSquares a b = (a * a) + (b * b)
 
--- 2. Defina a função `circleArea :: Float -> Float` que receba um raio r e calcule a área de um círculo com esse raio, dada por pi vezes o raio ao quadrado. Dica: Haskell tem a função `pi` pré-definida.
-circleArea :: Float -> Float
+circleArea :: Float -> Float 
+circleArea r = pi * r * r
 
--- 3. Defina uma função `age :: Int -> Int -> Int` que receba o ano de nascimento de uma pessoa e o ano atual, produzindo como resultado a idade (aproximada) da pessoa.
+age :: Int -> Int -> Int
+age year birth = year - birth
 
+isElderly :: Int -> Bool
+isElderly age = age > 65
 
--- 4. Defina uma função `isElderly :: Int -> Bool` que receba uma idade e resulte verdadeiro caso a idade seja maior que 65 anos.
+htmlItem :: String -> String
+htmlItem item = "<li>" ++ item ++ "</li>"
 
+startsWithA :: String -> Bool
+startsWithA word = (head word) == 'a'
 
--- 5. Defina uma função `htmlItem :: String -> String` que receba uma `String` e adicione tags `<li>` e `</li>` como prefixo e sufixo, respectivamente. Por exemplo, se a entrada for `"abc"`, a saída será `"<li>abc</li>"`. Use o operador `++` para concatenar strings (este operador serve para concatenar quaisquer listas do mesmo tipo).
+isVerb :: String -> Bool
+isVerb word = (last word) == 'r'
 
+isVowel :: Char -> Bool
+isVowel c
+  | c == 'a' = True
+  | c == 'e' = True
+  | c == 'i' = True
+  | c == 'o' = True
+  | c == 'u' = True
+  | otherwise = False
 
--- 6. Crie uma função `startsWithA :: String -> Bool` que receba uma string e verifique se ela inicia com o caracter `'A'`.
+hasEqHeads :: [Int] -> [Int] -> Bool
+hasEqHeads lst1 lst2 = head lst1 == head lst2
 
-
--- 7. Defina uma função `isVerb :: String -> Bool` que receba uma string e verifique se ela termina com o caracter `'r'`. Antes desse exercício, teste no interpretador a função pré-definida `last`, que retorna o último elemento de uma lista. Dica: conheça também o [list monster](http://s3.amazonaws.com/lyah/listmonster.png), do autor Miran Lipovača :-)
-
-
--- 8. Crie uma função `isVowel :: Char -> Bool` que receba um caracter e verifique se ele é uma vogal minúscula.
-
-
--- 9. Crie uma função `hasEqHeads :: [Int] -> [Int] -> Bool` que verifique se 2 listas possuem o mesmo primeiro elemento. Use a função `head` e o operador lógico `==` para verificar igualdade.
-
-
--- 10. Use a função `elem` para implementar uma função `isVowel2 :: Char -> Bool` que verifique se um caracter é uma vogal, tanto maiúscula como minúscula.
+isVowel2 :: Char -> Bool
+isVowel2 c = elem c "aeiou"
